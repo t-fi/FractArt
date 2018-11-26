2c1f3f5d2391f4300166f7b44b6fd1f5d5adb247
Tobi <tobias.fischer@posteo.net>
2018-11-26 10:34:52 +0100

    cranker shit

--- waves.py
+++ waves.py
@@ -7,27 +7,33 @@ import pycuda.autoinit
 from pycuda.compiler import SourceModule
 
 # TODO:
-#  mandel(center & zoom)
-#  interior_distance
-#  exterior_distance
+#   choose random point in [-2..1 + -1..1i]
+#   calculate exterior distance D => throw away if NaN
+#   Render img around point with zoom D
+#   Subsample img
+
+# TODO:
+#   move
 
 def mandel(center, zoom, resolution):
     grid_size = round(resolution / 32 + 0.5)
 
     KERNEL = f"""
+    #define prec double
+
     __global__ void mandel(float iterations[32*{grid_size}][32*{grid_size}]){{
         const int gid_x = threadIdx.x + blockIdx.x * blockDim.x;
         const int gid_y = threadIdx.y + blockIdx.y * blockDim.y;
 
-        const double c_im = {zoom:.16f}*(__int2float_rn(gid_x)/(32*{grid_size})-0.5) + {center[1]:.16f};
-        const double c_re = {zoom:.16f}*(__int2float_rn(gid_y)/(32*{grid_size})-0.5) + {center[0]:.16f};
+        const prec c_im = {zoom:.16f}*(__int2float_rn(gid_x)/(32*{grid_size})-0.5) + {center[1]:.16f};
+        const prec c_re = {zoom:.16f}*(__int2float_rn(gid_y)/(32*{grid_size})-0.5) + {center[0]:.16f};
 
         float iteration = 0;
 
-        double z_im = 0.;
-        double z_re = 0.;    
-        double z_im2 = 0.;
-        double z_re2 = 0.;    
+        prec z_im = 0.;
+        prec z_re = 0.;    
+        prec z_im2 = 0.;
+        prec z_re2 = 0.;    
 
         #pragma unroll
         for(unsigned i=0; i<10000; ++i){{
@@ -44,7 +50,7 @@ def mandel(center, zoom, resolution):
         }}
 
         if((z_re2 + z_im2) > 4){{
-            iterations[gid_x][gid_y] = sin(log(iteration + 1/log(2.) * log(log(100.)/(0.5*log(z_re2 + z_im2))))*5);
+            iterations[gid_x][gid_y] = log(iteration + 1/log(2.) * log(log(100.)/(0.5*log(z_re2 + z_im2))))*5;
         }} else {{
             iterations[gid_x][gid_y] = __int_as_float(0x7fffffff);
         }}
@@ -114,10 +120,10 @@ def mandel_distance(center, zoom, resolution):
 
     return b
 
-
-zoom = 0.01
-center = np.array([-1.1797, 0.31])
-iterations = mandel_distance(center, zoom, 4*1024)
+# for float min~0.0001
+zoom = 0.0000001
+center = np.array([0.3495374, 0.066426])
+iterations = mandel(center, zoom, 2*1024)
 
 
 plt.matshow(iterations, cmap='prism', extent=[center[0]-zoom/2, center[0]+zoom/2, center[1]-zoom/2, center[1]+zoom/2], origin='lower')
